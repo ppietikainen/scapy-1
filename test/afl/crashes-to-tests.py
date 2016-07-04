a49f621fa7aae94c38946ce000858f9e3a504cc6
Pekka Pietikäinen <pekka@pietikainen.org>
2016-07-04 10:35:33 +0300

    Cleanups

--- test/afl/crashes-to-tests.py
+++ test/afl/crashes-to-tests.py
@@ -1,6 +1,11 @@
 import os
 from scapy.all import *
 import traceback
+import json
+import struct
+
+from config import allowed
+ex = {}
 
 for root, dirs, files in os.walk('stub'):
     if 'crashes' in root:
@@ -10,14 +15,26 @@ for root, dirs, files in os.walk('stub'):
             for r in repros:
                 with  open("%s/%s" % (root, r)) as f:
                     repro = f.read()
-                print "%s(%s)" % (proto, repr(repro) )
+
                 try:
                     getattr(scapy.all, proto)(repro)
+                except struct.error:
+                    ex.setdefault(proto, set()).add("struct.error")
                 except Exception as e:
                     exc_type, exc_value, exc_traceback = sys.exc_info()
-#                    traceback.print_exc(file=sys.stdout)
-#                print "---"
+                    exname=exc_type.__name__
+                    if exname not in allowed:
+                        for l in traceback.format_exc().splitlines()[-5:]:
+                            print "# %s" % l
+                        print "%s(%s)\n" % (proto, repr(repro))
+                    ex.setdefault(proto, set()).add(str(exc_type.__name__))
 
+for k in ex.keys():
+    ex[k] = sorted(ex[k])
+
+with open("seen-crashes.json","w") as f:
+    json.dump(ex,f)
+    
 
       
         
